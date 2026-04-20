2f634e1232051293a002368bb38b76d80ce4699a
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-20 22:32:20 +0100

    new update

--- src/main/java/com/order_service/order_service/data/models/Order.java
+++ src/main/java/com/order_service/order_service/data/models/Order.java
@@ -1,6 +1,6 @@
 package com.order_service.order_service.data.models;
 
-import com.order_service.order_service.dtos.requests.ShippingAddressDto;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,19 +10,27 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private UUID buyerId;
     private UUID sellerId;
+
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;
     private Long totalPrice;
     private String currency;
     private String paymentMethod;
-    private ShippingAddressDto shippingAddress;
+    private String shippingAddress;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_id")
     private List<OrderItem> items;
     private String idempotencyKey;
     private LocalDateTime cancelledAt;
