e643adb495211434a0fa2f98f189946da046d642
Adamsonoladipupo <adamsonoladipupo@gmail.com>
2026-04-20 21:51:36 +0100

    updated the place order service

--- src/main/java/com/order_service/order_service/services/OrderService.java
+++ src/main/java/com/order_service/order_service/services/OrderService.java
@@ -1,22 +1,45 @@
 package com.order_service.order_service.services;
 
+import com.order_service.order_service.data.models.Order;
+import com.order_service.order_service.data.models.OrderStatus;
 import com.order_service.order_service.data.repositories.OrderRepository;
 import com.order_service.order_service.dtos.requests.PlaceOrderRequest;
 import com.order_service.order_service.dtos.responses.PlaceOrderResponse;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
+    private ModelMapper modelMapper;
 
     public PlaceOrderResponse placeOrder(PlaceOrderRequest request,
                                          UUID buyerId,
                                          String idempotencyKey){
-        
-        return null;
+        if (idempotencyKey != null) {
+            Optional<Order> existing = orderRepository.findByIdempotencyKey(idempotencyKey);
+            if (existing.isPresent()) {
+                return modelMapper.map(existing, PlaceOrderResponse.class);
+            }
+        }
+
+        long totalAmount = request.getItems().stream()
+                .mapToLong(item -> item.getUnitPrice() * item.getQty())
+                .sum();
+
+        Order order = new Order();
+        order.setBuyerId(buyerId);
+        order.setIdempotencyKey(idempotencyKey);
+        order.setStatus(OrderStatus.RESERVED);
+        order.setTotalPrice(totalAmount);
+        modelMapper.map(request, order);
+        orderRepository.save(order);
+
+        return modelMapper.map(order, PlaceOrderResponse.class);
     }
 }
